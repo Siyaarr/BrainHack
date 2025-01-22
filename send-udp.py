d1bcbe4b04bc22ad2ab35a4aec718c312c6266b5
Juho Kostet <juho@robotuprising.fi>
2025-01-22 15:42:34 +0200

    add example for sending movement command to robot

---
+++ send-udp.py
@@ -0,0 +1,7 @@
+import socket
+import time
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto(bytes("-100;-100", "utf-8"), ("192.168.255.95", 3000))
+time.sleep(1)
+sock.sendto(bytes("0;0", "utf-8"), ("192.168.255.95", 3000))
